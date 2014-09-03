c9ae7e08f5bb08b5776da4dc3387403d6279a4a3
Jason Palmer <jpalmer@spotify.com>
2014-09-02 20:40:49 -0400

    Initial checkin
    Change-Id: Ie5097f8b19070338ea92d019cde576a1a2133995

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/* jshint undef: true, unused: true */
+/* global module */
+'use strict';
+
+var pathogen = require('pathogen');
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    srcPkg: grunt.file.readJSON('./node_modules/dashjs/package.json'),
+
+    exec: {
+      srcDepInstall: {
+        cmd: 'cd ' + pathogen.sys('./node_modules/dashjs') + ' && npm install'
+      },
+      srcPkg: {
+        cmd: 'grunt --base ' + pathogen.sys('./node_modules/dashjs') + ' --gruntfile ' + pathogen.sys('./node_modules/dashjs/Gruntfile.js') + ' uglify'
+      },
+      cpSrcBundle: {
+        cmd: 'cp ' + pathogen.sys('./node_modules/dashjs/dash.all.js') + ' ' + pathogen.sys('./dashjs/') + 'index.js'
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-exec');
+
+  grunt.registerTask('default', ['exec:srcDepInstall', 'exec:srcPkg', 'exec:cpSrcBundle']);
+};
